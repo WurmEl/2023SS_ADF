f226269697de36d41ec6125ee50ff7864f1f7a27
WurmEl <FH@md60dnhc.ad001.siemens.net>
2023-03-08 16:36:05 +0100

    init

---
+++ HelloWorld.pas
@@ -0,0 +1,8 @@
+(* HelloWorld:                                                               *)
+(* -----------                                                               *)
+(* This is a simple hello world program to test the VS Code workspace.       *)
+(* ========================================================================= *)
+PROGRAM HelloWorld;
+BEGIN (* HelloWorld *)
+  WriteLn('Hello World!')
+END. (* HelloWorld *)
