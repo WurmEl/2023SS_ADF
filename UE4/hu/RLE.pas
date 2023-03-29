c628bfa51b4bfaf78500aed8936941a657a220be
WurmEl <elias.wurm01@gmail.com>
2023-03-29 18:28:16 +0200

    hu4 wip

---
+++ UE4/hu/RLE.pas
@@ -0,0 +1,92 @@
+(* RLE:                                               Wurm Elias, 2023-03-29 *)
+(* ------                                                                    *)
+(* run length encoding(RLE): is a method to compress text                    *)
+(* ========================================================================= *)
+
+program RLE;
+
+uses SysUtils;
+
+const
+  MAX_BUFFER_SIZE = 1024;
+
+type
+  RLEmode = (compress, decompress);
+
+function CompressString(var str: STRING): STRING;
+begin
+  writeln(str);
+  CompressString := str;
+end;
+
+function DecompressString(var str: STRING): STRING;
+begin
+  writeln(str);
+  DecompressString := str;
+end;
+
+procedure CompressFile(const InFileName, OutFileName: string);
+var
+  line: STRING;
+begin
+  line := 'compress';
+  CompressString(line);
+end;
+
+procedure DecompressFile(InFileName, OutFileName: string);
+var
+  line: STRING;
+begin
+  line := 'decompress';
+  DecompressString(line);
+end;
+
+var
+  Command: string;
+  InFileName, OutFileName: string;
+  mode: RLEmode;
+begin
+  if ParamCount > 0 then
+    Command := ParamStr(1)
+  else begin
+    write('enter if you want to compress (-c) or decompress (-d) > ');
+    ReadLn(Command);
+  end;
+  if (Command <> '-c') and (Command <> '-d') then
+  begin
+    WriteLn('Error: Unkowm Command - ', Command);
+    writeln;
+    Halt(1);
+  end;
+
+  if ParamCount > 1 then
+    InFileName := ParamStr(2)
+  else begin
+    write('enter infilename > ');
+    ReadLn(InFileName);
+  end;
+  if not FileExists(InFileName) then
+  begin
+    WriteLn('Error: input file does not exist - ', InFileName);
+    writeln;
+    Halt(1);
+  end;
+
+  if ParamCount > 2 then
+    OutFileName := ParamStr(3)
+  else begin
+    write('enter outfilename > ');
+    ReadLn(OutFileName);
+  end;
+  if (inFilename = outFilename) then
+  begin
+    WriteLn('Error: output file can not be the same as input file - ', InFileName);
+    writeln;
+    Halt(1);
+  end;
+
+  if Command = '-c' then
+    CompressFile(InFileName, OutFileName)
+  else
+    DecompressFile(InFileName, OutFileName);
+end.
